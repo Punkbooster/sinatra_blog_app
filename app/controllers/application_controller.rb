af99646783b7d998b6e2a5ea129de049ab7a971b
Punkbooster9000 <arsen.uhliar@beatport.com>
2025-08-31 16:43:23 +0200

    Add first migration and some initial views, controler, routes

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,22 @@
+class ApplicationController < Sinatra::Base
+
+  get "/" do
+    erb :"blogs/new"
+  end
+
+  get "/blogs/:id" do
+    @blog = Blog.find(params[:id])
+
+    erb :"blogs/show"
+  end
+
+  post "/" do
+    blog = Blog.new(params["blog"])
+
+    if blog.save
+      redirect "/blogs/#{blog.id}"
+    else
+      erb :"blogs/new"
+    end
+  end
+end
