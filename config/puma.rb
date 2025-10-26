a9a33f06440fbf6fa6177bc5d18b389087595f0d
Punkbooster9000 <arsen.uhliar@beatport.com>
2025-10-26 16:55:47 +0100

    Modify puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,4 @@
 environment ENV["RACK_ENV"] || "development"
-port        ENV["PORT"]     || 3000
 
 workers ENV.fetch("WEB_CONCURRENCY", 2)
 
@@ -7,8 +6,6 @@ threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
 threads threads_count, threads_count
 
 base_dir = "/home/arsen/projects/sinatra_blog_app"
-mkdir_p "#{base_dir}/run/sockets"
-mkdir_p "#{base_dir}/run/pids"
 
 bind "unix://#{base_dir}/run/sockets/puma.sock"
 
