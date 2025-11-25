c72afff7fc6fa834c3c621338be5f7315d10a33b
Punkbooster9000 <arsen.uhliar@beatport.com>
2025-11-25 21:40:24 +0100

    Add helpful debugging raise

--- config/database.rb
+++ config/database.rb
@@ -1,7 +1,12 @@
 if ENV.fetch("RACK_ENV", "development") == "production"
-  ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"])
+  database_url = ENV["DATABASE_URL"]
+
+  raise "DATABASE_URL environment variable is required in production." if database_url.blank?
+
+  ActiveRecord::Base.establish_connection(database_url)
 else
   database_name = "sinatra-blog-#{ApplicationController.environment}"
+
   ActiveRecord::Base.establish_connection(
     adapter: "postgresql",
     database: database_name,
