d734ff70b7d9fd00520df13b4a113240fd173f49
Punkbooster9000 <arsen.uhliar@beatport.com>
2025-11-25 21:06:12 +0100

    Change database.rb config

--- config/database.rb
+++ config/database.rb
@@ -1,15 +1,10 @@
-# set the database based on the current environment
-database_name = "sinatra-blog-#{ApplicationController.environment}"
-
-db = URI.parse(ENV["DATABASE_URL"] || "postgres://localhost/#{database_name}")
-
-# connect ActiveRecord with the current database
-ActiveRecord::Base.establish_connection(
-  :adapter => db.scheme == "postgres" ? "postgresql" : db.scheme,
-  :host => db.host,
-  :port => db.port,
-  :username => db.user,
-  :password => db.password,
-  :database => "#{database_name}",
-  :encoding => "utf8"
-)
+if ENV.fetch("RACK_ENV", "development") == "production"
+  ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"])
+else
+  database_name = "sinatra-blog-#{ApplicationController.environment}"
+  ActiveRecord::Base.establish_connection(
+    adapter: "postgresql",
+    database: database_name,
+    encoding: "utf8"
+  )
+end
