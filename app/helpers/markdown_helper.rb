1f7580e7415aa3ae669cd0ba794f6894055009be
Punkbooster9000 <arsen.uhliar@beatport.com>
2025-12-06 21:07:28 +0100

    Change markdown code formatter & add some style fixes

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -5,7 +5,7 @@ module MarkdownHelper
   def markdown_to_html(markdown)
     return "" if markdown.nil?
 
-    html = Commonmarker.to_html(markdown, plugins: { syntax_highlighter: { theme: "Solarized (dark)" } })
+    html = Commonmarker.to_html(markdown, plugins: { syntax_highlighter: { theme: "base16-eighties.dark" } })
 
     Sanitize.fragment(html, Sanitize::Config::RELAXED)
   end
